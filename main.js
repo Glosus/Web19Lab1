8f2ed07732ac1d601c5f8ad3e5153e53e017f6fb
Glosus <45399700+Glosus@users.noreply.github.com>
2019-09-19 23:53:49 +0300

    Final

---
+++ main.js
@@ -0,0 +1,72 @@
+function weatherSearch(city){
+  let xhr = new XMLHttpRequest();
+  let link = 'https://api.openweathermap.org/data/2.5/weather?';
+  let appid = '73a2f4a5254e97adabb2160caa6234a8'
+  let myRequest = link + 'q=' + city + '&appid=' + appid;
+  console.log(myRequest);
+  xhr.open('GET', myRequest, false);
+  xhr.send();
+
+  if (xhr.status != 200) {
+    console.log( xhr.status + ': ' + xhr.statusText );
+    return null;
+  } else {
+    let res = JSON.parse(xhr.responseText);
+    return res;
+  }
+}
+
+function weatherParser(resFromAPI){
+  let res = {};
+  let KelvinsInCelsius = 273.15;
+  res.temp = Math.round(parseFloat(resFromAPI.main.temp) - KelvinsInCelsius);
+  res.pressure = resFromAPI.main.pressure;
+  res.humidity = resFromAPI.main.humidity;
+  res.weather = resFromAPI.weather[0].description;
+  res.wind = resFromAPI.wind.speed;
+  return res;
+}
+
+function yTranslator(text, lang){
+  let xhr = new XMLHttpRequest();
+  let link = 'https://translate.yandex.net/api/v1.5/tr.json/translate?';
+  let key = 'trnsl.1.1.20190919T124743Z.dca6b4bd08046305.72d79d996d3b2d2aeda0f3e79b14b36111e3ef2c'
+  let myRequest = link + 'options=1' + '&key=' + key + '&text=' + text + '&lang=' + lang;
+
+  xhr.open('GET', myRequest, false);
+  xhr.send();
+
+  if (xhr.status != 200) {
+    console.log( xhr.status + ': ' + xhr.statusText );
+    return null;
+  } else {
+    let res = JSON.parse(xhr.responseText);
+    return res.text[0];
+  }
+}
+
+function resultsDisplay(param, value){
+  let aaa = doT.template(document.getElementById('my-test').innerHTML);
+  let label = param[0].toUpperCase() + param.slice(1) + ': ' + value;
+  let ara = aaa({
+      param: param,
+      value: yTranslator(label, 'ru')
+  	})
+    console.log(ara);
+  var d1 = document.getElementById('result_blocks');
+  d1.insertAdjacentHTML('beforeend', ara);
+
+}
+
+function main() {
+  document.getElementById('result_blocks').innerHTML = '';
+  let text = document.getElementsByName("search")[0].value;
+  let enSearchString = yTranslator(text, 'en');
+  console.log(enSearchString);
+  let weatherRes = weatherSearch(enSearchString);
+  console.log(weatherRes);
+  let finalData = weatherParser(weatherRes);
+  for (i in finalData) {
+    resultsDisplay(i, finalData[i])
+  }
+}
